6a7b5ec7e594cec1a2b47fbe2ea0d93aba280f7f
Man Behind MooN <man_behind_moon@gmx.net>
2022-03-28 23:01:27 +0200

    2.0.3 Fix 1

--- README.md
+++ README.md
@@ -1,35 +1,5 @@
-# <img src="plugin.video.srf_ch_replay/resources/icon.png" width="75" height="75" /> Unofficial SRF Replay (another official Kodi add-on)
+# <img src="plugin.video.srf_ch_replay/resources/icon.png" width="75" height="75" /> Unofficial SRF Replay
 
-## Migration
-This add-on has been migrated from [SRF Podcast Plugin](https://kodi.wiki/view/Add-on:SRF_Podcast_Plugin) [[source code](https://github.com/ambermoon/xbmc_plugin_video_srf_podcast_ch)] as its development seems to be on hold and unfortunately the add-on is not compatible with the newest Kodi version anymore. Thanks to all the previous developers who maintained the original add-on that I used and appreciated a lot.
-
-## Add-on description
-**This add-on is deprecated and will just be kept alive in its current state as long as the current used API is working. No new features will be added. A new version of this add-on "Unofficial SRG SSR Replay" has been launched that supports all the current features and some more. Information can be found under ["Unofficial SRG SSR Replay" Kodi Add-on Page](https://kodi.tv/addons/matrix/plugin.video.srgssr_ch_replay) or on ["Unofficial SRG SSR Replay" Github](https://github.com/ManBehindMooN/kodi_plugin_video_srgssr_ch_replay)**
-
-The add-on has been renamed to "Unofficial SRF Replay". It only supports the SRF channel and the only feature is to list and play this TV show library. All other channels and features from the original add-on have been refactored out. You might ask yourself 'Why?'. Well, never put avocado on a burger! Simple is always best!
-
-Since the tag 2.0.0 the add-on is in the official [Kodi 19 (Matrix) repository](https://github.com/xbmc/repo-plugins/tree/matrix/plugin.video.srf_ch_replay) / [Kodi Add-on Page](https://kodi.tv/addons/matrix/plugin.video.srf_ch_replay).
-
-## Usage
-The add-on can be used most efficiently in combination with favorites. When all the TV Shows are listed, just select your favorite show and open the Kodi context menu (press "c" on your keyboard) and select "Add to favorites".
-
-![Select favorites](pictures/usage1.png)
-
-The TV show just appears and the Kodi favorite menu and can be selected from there without going through the whole list every time.
-
-![Select favorites](pictures/usage2.png)
-
-Every time you select a TV show from your favorites only this show's content will be loaded. You will save click and loading time.
-
-## Installation
-
-### Kodi repository
-Go to the add-ons menu in your installed Kodi and select "Install from repository". Search for the add-on (just type "srf") and hit "install".
-
-### Manual
-If you want the latest features after the branch has been tagged then you need to install the code manually.
-
-Just zip the `plugin.video.srf_ch_replay` folder. Alternatively use the ant `build.xml` file and run the default `zip` target to build the zip file.
-
-Go to the add-ons menu and select "Install from zip file". Follow the instructions and at the end select the zip file and install. The "SRF Replay" add-on will appear immediately in your add-ons menu. If you get and error that your usb stick can not be read just restart Kodi and try again.
+**This 2.0.3-fix version on this branch has just been fixed for fun and will not be delivered to the official Kodi repository.**
+Only a manual installation is possible. For further information have a look at the [README on github](https://github.com/ManBehindMooN/kodi_plugin_video_srf_ch_replay/)
 
--- plugin.video.srf_ch_replay/addon.py
+++ plugin.video.srf_ch_replay/addon.py
@@ -9,60 +9,31 @@ import xbmc
 import xbmcplugin
 import xbmcgui
 import xbmcaddon
-import xbmcvfs
-#import os
 import traceback
 from io import StringIO
 import gzip
 from urllib.parse import urlparse
 from string import ascii_lowercase
 
-#'Base settings'
-#'Start of the plugin functionality is at the end of the file'
 addon = xbmcaddon.Addon()
-addonID = 'plugin.video.srf_ch_replay'
+#addonID = 'plugin.video.srf_ch_replay'
 pluginhandle = int(sys.argv[1])
 socket.setdefaulttimeout(30)
 xbmcplugin.setPluginCategory(pluginhandle, "News")
 xbmcplugin.setContent(pluginhandle, "tvshows")
-#addon_work_folder = xbmcvfs.translatePath("special://profile/addon_data/" + addonID)
-#if not os.path.isdir(addon_work_folder):
-#    os.mkdir(addon_work_folder)
-FavoritesFile = xbmcvfs.translatePath("special://profile/addon_data/" + addonID + "/" + addonID + ".favorites")
 numberOfEpisodesPerPage = str(addon.getSetting("numberOfShowsPerPage"))
 tr = addon.getLocalizedString
-
-#####################################
-# OLD SRF Podcast Plugin api methods
-#####################################
-
-def open_srf_url(urlstring):
-    request = urllib.request.Request(urlstring)
-    request.add_header('Accept-encoding', 'gzip')
-    response = ''
-    try:
-        response = urllib.request.urlopen(urlstring)
-        if response.info().get('Content-Encoding') == 'gzip':
-            buf = StringIO(response.read())
-            f = gzip.GzipFile(fileobj=buf)
-            response = StringIO(f.read())
-    except Exception as e:
-        xbmc.log(traceback.format_exc())
-        xbmcgui.Dialog().ok(tr(30006), str(e.__class__.__name__), str(e))
-    return response
-
 
 def list_all_tv_shows(letter):
     """
     this method list all available TV shows
     """
     url = 'https://il.srgssr.ch/integrationlayer/2.0/srf/showList/tv/alphabetical?vector=portalplay&pageSize=unlimited&onlyActiveShows=false'
-    response = json.load(open_srf_url(url))
+    response = json.load(_open_srf_url(url))
     shows = response["showList"]
     title = ''
     desc = ''
     picture = ''
-    page = 1
     mode = 'listEpisodes'
     for show in shows:
         try:
@@ -80,18 +51,18 @@ def list_all_tv_shows(letter):
 
         firstTitleLetter = title[:1]
         if (firstTitleLetter.lower() == letter) or (not firstTitleLetter.isalpha() and not str(letter).isalpha()):
-            _add_show(title, show['id'], mode, desc, picture, page)
+            _add_show(title, show['id'], mode, desc, picture)
 
     xbmcplugin.addSortMethod(pluginhandle, 1)
     xbmcplugin.endOfDirectory(pluginhandle)
     xbmcplugin.endOfDirectory(handle=pluginhandle, succeeded=True)
 
 
-def _add_show(name, url, mode, desc, iconimage, page):
+def _add_show(name, url, mode, desc, iconimage):
     """
     helper method to create a folder with subitems
     """
-    directoryurl = sys.argv[0] + "?url=" + urllib.parse.quote_plus(url) + "&mode=" + str(mode) + "&showbackground=" + urllib.parse.quote_plus(iconimage) + "&page=" + str(page) + "&channel=srf"
+    directoryurl = sys.argv[0] + "?url=" + urllib.parse.quote_plus(url) + "&mode=" + str(mode) + "&showbackground=" + urllib.parse.quote_plus(iconimage)
     liz = xbmcgui.ListItem(name)
     liz.setLabel2(desc)
     liz.setArt({'poster': iconimage, 'banner': iconimage, 'fanart': iconimage, 'thumb': iconimage})
@@ -101,11 +72,63 @@ def _add_show(name, url, mode, desc, iconimage, page):
     return ok
 
 
+def list_all_episodes(showid, showbackground, nextPageUrl):
+    """
+    this method list all episodes of the selected show
+    """
+
+    response = ''
+    if nextPageUrl == '':
+        url = 'https://il.srgssr.ch/integrationlayer/2.0/srf/mediaList/video/latest/byShow/' + showid + '.json?pageSize=' + str(numberOfEpisodesPerPage)
+        response = json.load(_open_srf_url(url))
+    else:
+        response = json.load(_open_srf_url(nextPageUrl))
+
+    show = response["mediaList"]
+
+    for episode in show:
+        title = episode['title']
+        urn = ''
+        desc = ''
+        picture = ''
+        pubdate = episode['episode']['publishedDate']
+
+        try:
+            desc = episode['episode']['description']
+        except:
+            desc = tr(30008)
+        try:
+            picture = episode['imageUrl']
+        except:
+            # no picture
+            picture = ''
+        try:
+            length = int(episode['duration']) / 1000 / 60
+        except:
+            length = 0
+        try:
+            urn = episode['urn']
+        except:
+            urn = tr(30009)
+        try:
+            titleextended = ' - ' + episode['lead']
+        except:
+            titleextended = ''
+
+        _addLink(title + titleextended, urn, 'playepisode', desc, picture, length, pubdate, showbackground)
+
+    # check if another page is available
+    try:
+        _addnextpage(tr(30005), showid, 'listEpisodes', showbackground, response["next"])
+    finally:
+        xbmcplugin.endOfDirectory(pluginhandle)
+
+
 def _addLink(name, url, mode, desc, iconurl, length, pubdate, showbackground):
     """
     helper method to create an item in the list
     """
-    linkurl = sys.argv[0] + "?url=" + urllib.parse.quote_plus(url) + "&mode=" + str(mode) + "&channel=srf"
+    linkurl = sys.argv[0] + "?url=" + urllib.parse.quote_plus(url) + "&mode=" + str(mode)
     liz = xbmcgui.ListItem(name)
     liz.setLabel2(desc)
     liz.setArt({'poster': iconurl, 'banner': iconurl, 'fanart': showbackground, 'thumb': iconurl})
@@ -116,126 +139,88 @@ def _addLink(name, url, mode, desc, iconurl, length, pubdate, showbackground):
     return ok
 
 
-def _addnextpage(name, url, mode, desc, showbackground, page):
+def _addnextpage(name, url, mode, showbackground, nextPageUrl):
     """
     helper method to create a folder with subitems
     """
-    directoryurl = sys.argv[0] + "?url=" + urllib.parse.quote_plus(url) + "&mode=" + str(mode) + "&showbackground=" + urllib.parse.quote_plus(showbackground) + "&page=" + str(page) + "&channel=srf"
+    directoryurl = sys.argv[0] + "?url=" + urllib.parse.quote_plus(url) + "&mode=" + str(mode) + "&showbackground=" + urllib.parse.quote_plus(showbackground) + "&nextPage=" + urllib.parse.quote_plus(nextPageUrl)
     liz = xbmcgui.ListItem(name)
-    liz.setLabel2(desc)
-    #liz.setArt({'poster' : '' , 'banner' : '', 'fanart' : showbackground, 'thumb' : ''})
-    liz.setInfo(type="Video", infoLabels={"title": name, "plot": desc, "plotoutline": desc})
+    liz.setInfo(type="Video", infoLabels={"title": name})
     xbmcplugin.setContent(pluginhandle, 'episodes')
     ok = xbmcplugin.addDirectoryItem(pluginhandle, url=directoryurl, listitem=liz, isFolder=True)
     return ok
 
 
-def list_all_episodes(showid, showbackground, page):
-    """
-    this method list all episodes of the selected show
-    """
-    url = 'http://il.srgssr.ch/integrationlayer/1.0/ue/srf/assetSet/listByAssetGroup/' + showid + '.json?pageNumber=' + str(page) + "&pageSize=" + str(numberOfEpisodesPerPage)
-    response = json.load(open_srf_url(url))
-    maxpage = 1
-    try:
-        maxpage = response["AssetSets"]["@maxPageNumber"]
-    except:
-        maxpage = 0
-
-    show = response["AssetSets"]["AssetSet"]
-
-    for episode in show:
-        title = episode['title']
-        url = ''
-        desc = ''
-        picture = ''
-        pubdate = episode['publishedDate']
-
-        try:
-            desc = episode['Assets']['Video'][0]['AssetMetadatas']['AssetMetadata'][0]['description']
-        except:
-            desc = tr(30008)
-        try:
-            picture = episode['Assets']['Video'][0]['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
-        except:
-            # no picture
-            picture = ''
-        try:
-            length = int(episode['Assets']['Video'][0]['duration']) / 1000 / 60
-        except:
-            length = 0
-        try:
-            url = episode['Assets']['Video'][0]['id']
-        except:
-            url = tr(30009)
-        try:
-            titleextended = ' - ' + episode['Assets']['Video'][0]['AssetMetadatas']['AssetMetadata'][0]['title']
-        except:
-            titleextended = ''
-
-        _addLink(title + titleextended, url, 'playepisode', desc, picture, length, pubdate, showbackground)
-
-    # check if another page is available
-    page = int(page)
-    maxpage = int(maxpage)
-    if page < maxpage or maxpage == 0 and len(show) == int(numberOfEpisodesPerPage):
-        page = page + 1
-        _addnextpage(tr(30005).format(page, maxpage), showid, 'listEpisodes', '', showbackground, page)
-        
-    xbmcplugin.endOfDirectory(pluginhandle)
-
-
-def playepisode(episodeid):
+def playepisode(urn):
     """
     this method plays the selected episode
     """
 
-    besturl = ''
-
-    try:
-        url = 'http://il.srgssr.ch/integrationlayer/1.0/ue/srf/video/play/' + episodeid + '.json'
-        response = json.load(open_srf_url(url))
-        playlistVector = response['Video']['Playlists']['Playlist']
-
-        # filter objects with list comprehensions
-        playlist = [obj for obj in playlistVector if obj['@protocol'] == 'HTTP-HLS']
-
-        playlistVector = playlist[0]
-        urls = playlistVector['url']
-
-        besturl = urls[0]['text']
-        for tempurl in urls:
-            if tempurl['@quality'] == 'HD':
-                besturl = tempurl['text']
-                break
-
-    except:
-        xbmc.log(traceback.format_exc())
-
-    if besturl == '':
-        try:
-            url = 'http://il.srgssr.ch/integrationlayer/1.0/ue/srf/video/play/' + episodeid + '.json'
-            response = json.load(open_srf_url(url))
-            urls = response['Video']['Playlists']['Playlist'][0]['url']
-
-            besturl = urls[0]['text']
-            for tempurl in urls:
-                if tempurl['@quality'] == 'HD':
-                    besturl = tempurl['text']
-                    break
-
-        except:
-            xbmc.log(traceback.format_exc())
+    besturl = _parse_integrationplayer_2(urn)
 
     # add authentication token for akamaihd
     if "akamaihd" in urlparse(besturl).netloc:
         url = "http://tp.srgssr.ch/akahd/token?acl=" + urlparse(besturl).path
-        response = json.load(open_srf_url(url))
+        response = json.load(_open_srf_url(url))
         token = response["token"]["authparams"]
         besturl = besturl + '?' + token
 
     listitem = xbmcgui.ListItem(path=besturl)
     xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+
+
+def _parse_integrationplayer_2(urn):
+    integrationlayerUrl = f'https://il.srgssr.ch/integrationlayer/2.0/mediaComposition/byUrn/{urn}.json'
+    response = json.load(_open_srf_url(integrationlayerUrl))
+
+    resourceList = response['chapterList'][0]['resourceList']
+    sdHlsUrls = []
+    for play in resourceList:
+        if play['protocol'] == 'HLS':
+            if play['quality'] == 'HD':
+                return _remove_params(play['url'])
+            else:
+                sdHlsUrls.append(play)
+
+    if not sdHlsUrls:
+        return _remove_params(resourceList[0]['url'])
+    else:
+        return _remove_params(sdHlsUrls[0]['url'])
+
+
+def _remove_params(url):
+    parsed = urlparse(url)
+    return f'{parsed.scheme}://{parsed.netloc}{parsed.path}'
+
+
+def _open_srf_url(urlstring):
+    request = urllib.request.Request(urlstring)
+    request.add_header('Accept-encoding', 'gzip')
+    response = ''
+    try:
+        response = urllib.request.urlopen(urlstring)
+        if response.info().get('Content-Encoding') == 'gzip':
+            buf = StringIO(response.read())
+            f = gzip.GzipFile(fileobj=buf)
+            response = StringIO(f.read())
+    except Exception as e:
+        xbmc.log(traceback.format_exc())
+        xbmcgui.Dialog().ok(tr(30006), str(e))
+    return response
+
+
+def choose_tv_show_letter():
+    nextMode = 'listTvShows'
+    _add_letter('#', tr(30019), nextMode)
+    for c in ascii_lowercase:
+        _add_letter(c, c, nextMode)
+    xbmcplugin.endOfDirectory(handle=pluginhandle, succeeded=True)
+
+
+def _add_letter(letter, letterDescription, mode):
+    directoryurl = sys.argv[0] + "?mode=" + str(mode) + "&letter=" + letter
+    liz = xbmcgui.ListItem(letterDescription)
+    return xbmcplugin.addDirectoryItem(pluginhandle, url=directoryurl, listitem=liz, isFolder=True)
 
 
 def _parameters_string_to_dict(parameters):
@@ -251,36 +236,20 @@ def _parameters_string_to_dict(parameters):
                 paramDict[paramSplits[0]] = paramSplits[1]
     return paramDict
 
-def choose_tv_show_letter(channel):
-    nextMode = 'listTvShows'
-    _add_letter(channel, '#', tr(30019), nextMode)
-    for c in ascii_lowercase:
-        _add_letter(channel, c, c, nextMode)
-    xbmcplugin.endOfDirectory(handle=pluginhandle, succeeded=True)
-
-
-def _add_letter(channel, letter, letterDescription, mode):
-    directoryurl = sys.argv[0] + "?mode=" + str(mode) + "&channel=" + str(channel) + "&letter=" + letter
-    liz = xbmcgui.ListItem(letterDescription)
-    return xbmcplugin.addDirectoryItem(pluginhandle, url=directoryurl, listitem=liz, isFolder=True)
-
 
 #'Start'
 params = _parameters_string_to_dict(sys.argv[2])
 mode = params.get('mode', '')
 url = params.get('url', '')
 showbackground = urllib.parse.unquote_plus(params.get('showbackground', ''))
-page = params.get('page', '')
-channel = params.get('channel', 'srf')
+nextPage = urllib.parse.unquote_plus(params.get('nextPage', ''))
 letter = params.get('letter', '')
 
 if mode == 'playepisode':
     playepisode(url)
 elif mode == 'listEpisodes':
-    list_all_episodes(url, showbackground, page)
+    list_all_episodes(url, showbackground, nextPage)
 elif mode == 'listTvShows':
     list_all_tv_shows(letter)
 else:
-    choose_tv_show_letter(channel)
-
-    
+    choose_tv_show_letter()
